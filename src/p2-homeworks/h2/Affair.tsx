3b0e11fbf80d4897a22a513ac00e378a90d995c3
ZherdevMak <Zherdev.mak@gmail.com>
2022-05-31 12:49:41 +0300

    Home Work2 done

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import {AffairType} from "./HW2";
-
+import css from './affair.module.css'
 type AffairPropsType = {
     // key не нужно типизировать
     key1: any
     affair: AffairType// need to fix any
-    deleteAffairCallback: (_id:number) => void
+    deleteAffairCallback: (_id: number) => void
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {props.deleteAffairCallback(props.affair._id)}// need to fix
+    const deleteCallback = () => {
+        props.deleteAffairCallback(props.affair._id)
+    }// need to fix
     return (
-        <div key={props.key1}>
-            <span>{props.affair._id} {props.affair.name} {props.affair.priority}</span>
-            <button onClick={deleteCallback}>X</button>
-        </div>
+        <table className={css.affair_table} key={props.key1}>
+            <tbody>
+            <tr>
+                <th className={css.colomn}>
+                    <span>{props.affair._id}</span>
+                </th>
+                <th className={css.colomn}>
+                    <span>{props.affair.name}</span>
+                </th>
+                <th className={css.colomn_3}>
+                    <span>{props.affair.priority}</span>
+                    <button onClick={deleteCallback}>X</button>
+                </th>
+            </tr>
+            </tbody>
+        </table>
     )
 }
 
--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Affair from './Affair'
 import {AffairType, FilterType} from './HW2'
+import css from './Affairs.module.css'
 
 type AffairsPropsType = { // need to fix any
     data: AffairType[]
@@ -23,14 +24,14 @@ function Affairs(props: AffairsPropsType) {
     const setLow = () => {props.setFilter('low')}
 
     return (
-        <div key="setAll">
+        <div className={css.affairs} key={'1'}>
 
             {mappedAffairs}
 
-            <button  onClick={setAll}>All</button>
-            <button  onClick={setHigh}>High</button>
-            <button  onClick={setMiddle}>Middle</button>
-            <button  onClick={setLow}>Low</button>
+            <button className={css.button}  onClick={setAll}>All</button>
+            <button className={css.button} onClick={setHigh}>High</button>
+            <button className={css.button} onClick={setMiddle}>Middle</button>
+            <button className={css.button} onClick={setLow}>Low</button>
         </div>
     )
 }
