d269d58cca714143909f5c3b6ce30cf4fa32b777
ZherdevMak <Zherdev.mak@gmail.com>
2022-07-15 17:33:17 +0300

    HW 12 theme change

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import s from "./HW12.module.css";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../h10/bll/store";
+import {changeThemeAC, initStateProps} from "./bll/themeReducer";
+import SuperRadio from "../h7/common/c6-SuperRadio/SuperRadio";
+import {arrType} from "../h7/HW7";
 
-const themes = ['dark', 'red', 'some'];
+const themes:arrType = ['dark', 'red', 'defolt'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
-
-    // useDispatch, onChangeCallback
+    const themeState = useSelector<AppStoreType, initStateProps>(state => state.theme)
+    const dispatch = useDispatch()
+    const theme = themeState.theme
+    const onChangeOption = (value:string) => {
+        dispatch(changeThemeAC(value))
+    }
 
     return (
         <div className={s[theme]}>
@@ -16,7 +24,11 @@ function HW12() {
             </span>
 
             {/*should work (должно работать)*/}
-            {/*SuperSelect or SuperRadio*/}
+            <SuperRadio
+                name={'radio'}
+                options={themes}
+                value={theme}
+                onChangeOption={onChangeOption}></SuperRadio>
 
             <hr/>
         </div>
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,21 @@
-const initState = {
 
-};
+export type initStateProps = {theme:string}
+export const initState:initStateProps ={theme:'defolt'}
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+
+
+export const themeReducer = (state:initStateProps = initState, action: changeThemeACType): initStateProps => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case "TOGGLE_OF_THEME": {
+            return {...state,theme:action.themeName};
         }
         default: return state;
     }
 };
-
-export const changeThemeC = (): any => {}; // fix any
+type changeThemeACType = ReturnType<typeof changeThemeAC>
+export const changeThemeAC = (themeName:string) => {
+    return{
+        type: 'TOGGLE_OF_THEME',
+        themeName:themeName
+    } as const
+}; // fix any
